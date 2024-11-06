1db302e37c3e378c05d5861d30e89c257b17f52f
Tods <algy25ng@gmail.com>
2024-11-06 22:28:23 +0800

    s3 expire 7 hari

--- src/services/S3/StorageService.js
+++ src/services/S3/StorageService.js
@@ -32,7 +32,7 @@ class StorageService {
 
   createPreSignedUrl({ bucket, key }) {
     const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-    return getSignedUrl(this._S3, command, { expiresIn: 31536000 });
+    return getSignedUrl(this._S3, command, { expiresIn: 604800 });
   }
 }
 
